d8e9989b95a15686d4a3aa46d6f4eefab38fec4f
chrys-exaucet <chrysexaucet@hotmail.fr>
2021-01-03 23:45:53 +0000

    added keapser submodules

--- keapser-service/src/main/java/keapser/chat/KeapserServiceApplication.java
+++ keapser-service/src/main/java/keapser/chat/KeapserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeapserChatApplication  {
+public class KeapserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KeapserChatApplication.class, args);
+		SpringApplication.run(KeapserServiceApplication.class, args);
 	}
 
 }
