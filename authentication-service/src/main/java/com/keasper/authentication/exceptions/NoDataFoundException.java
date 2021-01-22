c02adf9f846fb65de55558a88e18ad48915605b1
salomon-dhn <salomondahoun@yahoo.fr>
2021-01-22 14:29:56 +0100

    Add Exceptions and Constraints

---
+++ authentication-service/src/main/java/com/keasper/authentication/exceptions/NoDataFoundException.java
@@ -0,0 +1,12 @@
+package com.keasper.authentication.exceptions;
+
+@SuppressWarnings("serial")
+public class NoDataFoundException extends RuntimeException {
+
+	public NoDataFoundException() {
+		super("No Data Found");
+		
+	}
+
+	
+}
