5f5dcf1fdb86f79f0dc0c3cfe343f3de300c5aec
leuk7 <jeanlucsanou@gmail.com>
2021-01-24 19:42:41 +0000

    Rebase with the last push

---
+++ ui/src/components/AccountCreation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./AccountCreation.css"
+
+function AccountCreation() {
+    return (
+        <div className="accountCreation">
+            <div className="container"></div>
+        </div>
+    )
+}
+
+export default AccountCreation
